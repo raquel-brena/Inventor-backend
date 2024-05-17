28d8cb66741301b2bd8d8bb656971f622d2a4095
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-16 23:50:07 -0300

    feat: add product controller

--- src/main/java/com/rb/auth/controllers/ProductController.java
+++ src/main/java/com/rb/auth/controllers/ProductController.java
@@ -1,15 +1,16 @@
 package com.rb.auth.controllers;
 
-
 import com.rb.auth.domain.product.Product;
 import com.rb.auth.domain.product.ProductRequestDTO;
+import com.rb.auth.domain.product.ProductResponseDTO;
 import com.rb.auth.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("product")
@@ -17,11 +18,18 @@ public class ProductController {
 
     @Autowired
     ProductRepository repository;
+    @PostMapping
+    public ResponseEntity postProduct(@RequestBody @Validated ProductRequestDTO body){
+        Product newProduct = new Product(body);
 
-    public ResponseEntity createProduct (@RequestBody @Validated ProductRequestDTO body) {
-
-        this.repository.save(new Product(body));
-
+        this.repository.save(newProduct);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        List<ProductResponseDTO> productList = this.repository.findAll().stream().map(ProductResponseDTO::new).toList();
+
+        return ResponseEntity.ok(productList);
     }
 }
