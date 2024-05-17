37164d3c7f78b41c56a8ce500efa987d86f941f6
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-16 23:52:20 -0300

    feat: add auth controller

---
+++ src/main/java/com/rb/auth/controllers/AuthenticationController.java
@@ -0,0 +1,47 @@
+package com.rb.auth.controllers;
+
+import com.rb.auth.domain.user.AuthenticatedDTO;
+import com.rb.auth.domain.user.RegisterDTO;
+import com.rb.auth.domain.user.User;
+import com.rb.auth.repositories.UserRepository;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository repository;
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticatedDTO data){
+        var userNamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+
+        var auth = this.authenticationManager.authenticate(userNamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+     if (this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+     String encryptedPassword = new BCryptPasswordEncoder().encode((data.password()));
+
+     this.repository.save( new User(data.login(),encryptedPassword, data.role()));
+     return ResponseEntity.ok().build();
+    }
+
+}
