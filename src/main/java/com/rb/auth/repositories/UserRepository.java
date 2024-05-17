d1f411c030b14842cd4c6758285d4dd78b30103d
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-16 23:48:32 -0300

    feat: add User repository

---
+++ src/main/java/com/rb/auth/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rb.auth.repositories;
+
+import com.rb.auth.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByLogin(String login);
+}
