e94e49d78a5bc416e23a1f7da1a391d644ff4b92
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-17 21:59:33 -0300

    fix: fix request matchers

--- src/main/java/com/rb/auth/infra/security/SecurityConfiguration.java
+++ src/main/java/com/rb/auth/infra/security/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "product").hasRole("ADMÌN")
+                        .requestMatchers(HttpMethod.POST, "product").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated())
