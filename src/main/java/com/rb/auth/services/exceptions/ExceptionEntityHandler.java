e556a7fd37c42deebc34370b3a2c4a55138fd329
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-20 21:24:42 -0300

    feat: add exception handler for ProductNotFoundException

---
+++ src/main/java/com/rb/auth/services/exceptions/ExceptionEntityHandler.java
@@ -0,0 +1,16 @@
+package com.rb.auth.services.exceptions;
+
+import com.rb.auth.services.exceptions.product.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionEntityHandler  {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
