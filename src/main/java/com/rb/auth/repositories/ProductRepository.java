665975421915958bee30400b616618a55b2bb5c7
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-20 21:25:10 -0300

    feat: Add findByCategory method to ProductRepository

--- src/main/java/com/rb/auth/repositories/ProductRepository.java
+++ src/main/java/com/rb/auth/repositories/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.rb.auth.repositories;
 
+import com.rb.auth.domain.product.Category;
 import com.rb.auth.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findById(String id);
+    Optional<Product> findBySku(String sku);
+   List<Product> findByCategory(Category category);
+
 }
