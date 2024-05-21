717cfe987b9dd2fa2ee2b67c808821921604d2b2
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-20 21:26:59 -0300

    feat: add Product class

--- src/main/java/com/rb/auth/domain/product/Product.java
+++ src/main/java/com/rb/auth/domain/product/Product.java
@@ -1,28 +1,42 @@
 package com.rb.auth.domain.product;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Table(name = "product")
 @Entity(name = "product")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Product {
-
     @Id
-    @GeneratedValue (strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(nullable = false)
     private String name;
+
+    private String description;
+
+    @Column(unique = true, nullable = false)
+    private String sku;
+
+    @Column(nullable = false)
     private Integer price;
 
+    private Category category;
 
-    public Product(ProductRequestDTO data) {
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Product(ProductRequestDTO data){
         this.price = data.price();
         this.name = data.name();
+        this.description = data.description();
+        this.category = data.category();
+        this.sku = data.sku();
+        this.quantity = data.quantity();
     }
 }
