d5c05c4b4df5525f664c587231ce223fde80de19
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-05-20 21:26:20 -0300

    feat: Add updateProduct, getProduct and getByCategory methods

--- src/main/java/com/rb/auth/controllers/ProductController.java
+++ src/main/java/com/rb/auth/controllers/ProductController.java
@@ -1,35 +1,82 @@
 package com.rb.auth.controllers;
 
+import com.rb.auth.domain.product.Category;
 import com.rb.auth.domain.product.Product;
 import com.rb.auth.domain.product.ProductRequestDTO;
 import com.rb.auth.domain.product.ProductResponseDTO;
 import com.rb.auth.repositories.ProductRepository;
+import com.rb.auth.services.exceptions.product.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("product")
+@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
     ProductRepository repository;
+
     @PostMapping
     public ResponseEntity postProduct(@RequestBody @Validated ProductRequestDTO body){
-        Product newProduct = new Product(body);
+        var product = this.repository.findBySku(body.sku());
+        if (product.isEmpty()) {
+            Product newProduct = new Product(body);
 
-        this.repository.save(newProduct);
-        return ResponseEntity.ok().build();
+            this.repository.save(newProduct);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping
+    public ResponseEntity updateProduct(@RequestBody @Validated ProductRequestDTO body){
+        System.out.println(body.sku());
+        var product = this.repository.findBySku(body.sku());
+        if (product.isPresent()) {
+            Product existingProduct = product.get();
+            existingProduct.setName(body.name());
+            existingProduct.setPrice(body.price());
+            existingProduct.setCategory(body.category());
+            existingProduct.setSku(body.sku());
+            existingProduct.setDescription(body.description());
+            existingProduct.setQuantity(body.quantity());
+            this.repository.save(existingProduct);
+            return ResponseEntity.status(201).build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> getProduct(@PathVariable String id) {
+        var product = this.repository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
+
+        ProductResponseDTO productDto = new ProductResponseDTO(product);
+        return ResponseEntity.ok(productDto);
     }
 
     @GetMapping
     public ResponseEntity getAllProducts(){
-        List<ProductResponseDTO> productList = this.repository.findAll().stream().map(ProductResponseDTO::new).toList();
+        System.out.println("pegando produtos");
+        List<ProductResponseDTO> productList = this.repository.findAll()
+                .stream()
+                .map(ProductResponseDTO::new)
+                .toList();
 
         return ResponseEntity.ok(productList);
+    }
+    @GetMapping("/{category}")
+    public ResponseEntity getByCategory (@PathVariable Category category) {
+        List<ProductResponseDTO> products = this.repository.findByCategory(category)
+                .stream()
+                .map(ProductResponseDTO::new)
+                .toList();
+
+        return ResponseEntity.ok(products);
     }
 }
