acc49ea93f814d0825f0a282adced73e44efe845
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-09-15 14:52:30 -0300

    Refactor ProductController to use enums for Category and update request/response DTOs

--- src/main/java/com/rb/auth/controllers/ProductController.java
+++ src/main/java/com/rb/auth/controllers/ProductController.java
@@ -1,9 +1,9 @@
 package com.rb.auth.controllers;
 
-import com.rb.auth.domain.product.Category;
+import com.rb.auth.domain.enums.Category;
 import com.rb.auth.domain.product.Product;
-import com.rb.auth.domain.product.ProductRequestDTO;
-import com.rb.auth.domain.product.ProductResponseDTO;
+import com.rb.auth.domain.product.CreateProductRequestDTO;
+import com.rb.auth.domain.product.CreateProductResponseDTO;
 import com.rb.auth.repositories.ProductRepository;
 import com.rb.auth.services.exceptions.product.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +15,14 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api/product")
 public class ProductController {
 
     @Autowired
     ProductRepository repository;
 
     @PostMapping
-    public ResponseEntity postProduct(@RequestBody @Validated ProductRequestDTO body){
+    public ResponseEntity<CreateProductResponseDTO> createProduct(@RequestBody @Validated CreateProductRequestDTO body){
         var product = this.repository.findBySku(body.sku());
         if (product.isEmpty()) {
             Product newProduct = new Product(body);
@@ -34,47 +34,48 @@ public class ProductController {
     }
 
     @PutMapping
-    public ResponseEntity updateProduct(@RequestBody @Validated ProductRequestDTO body){
+    public ResponseEntity updateProduct(@RequestBody @Validated CreateProductRequestDTO body){
         System.out.println(body.sku());
         var product = this.repository.findBySku(body.sku());
+
         if (product.isPresent()) {
             Product existingProduct = product.get();
             existingProduct.setName(body.name());
-            existingProduct.setPrice(body.price());
             existingProduct.setCategory(body.category());
             existingProduct.setSku(body.sku());
             existingProduct.setDescription(body.description());
-            existingProduct.setQuantity(body.quantity());
             this.repository.save(existingProduct);
-            return ResponseEntity.status(201).build();
+
+            return ResponseEntity.status(200).build();
         }
         return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> getProduct(@PathVariable String id) {
+    public ResponseEntity<CreateProductResponseDTO> getProduct(@PathVariable String id) {
         var product = this.repository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
 
-        ProductResponseDTO productDto = new ProductResponseDTO(product);
+        CreateProductResponseDTO productDto = new CreateProductResponseDTO(product);
         return ResponseEntity.ok(productDto);
     }
 
     @GetMapping
     public ResponseEntity getAllProducts(){
         System.out.println("pegando produtos");
-        List<ProductResponseDTO> productList = this.repository.findAll()
+        List<CreateProductResponseDTO> productList = this.repository.findAll()
                 .stream()
-                .map(ProductResponseDTO::new)
+                .map(CreateProductResponseDTO::new)
                 .toList();
 
         return ResponseEntity.ok(productList);
     }
+
     @GetMapping("/{category}")
     public ResponseEntity getByCategory (@PathVariable Category category) {
-        List<ProductResponseDTO> products = this.repository.findByCategory(category)
+        List<CreateProductResponseDTO> products = this.repository.findByCategory(category)
                 .stream()
-                .map(ProductResponseDTO::new)
+                .map(CreateProductResponseDTO::new)
                 .toList();
 
         return ResponseEntity.ok(products);
